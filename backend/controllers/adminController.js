02d5dac1f497826a82767aed406436f13e132aaa
Laxman Singh Koranga <korangalaxman03@gmail.com>
2024-11-26 21:28:09 +0530

    [Doctalk:backend]: Updated files.

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,3 +1,8 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+
 // API for adding doctor
 const addDoctor = async (req, res) => {
   try {
@@ -15,21 +20,68 @@ const addDoctor = async (req, res) => {
 
     const imageFile = req.file;
 
-    console.log(
-      {
-        name,
-        email,
-        password,
-        speciality,
-        degree,
-        experience,
-        about,
-        fees,
-        address,
-      },
-      imageFile
-    );
-  } catch (error) {}
+    // Checking for all data to add doctor.
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !address
+    ) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+
+    // validating email format.
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please, Enter a valid email",
+      });
+    }
+
+    // validating password strength.
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please, Enter a strong password",
+      });
+    }
+
+    // hashing doctor password.
+    const salt = await bcrypt.genSalt(9);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // upload image to cloudinary.
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+
+    res.json({ success: true, message: "Doctor Added." });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 export { addDoctor };
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -5,3 +5,5 @@ import upload from "../middlewares/multer.js";
 const adminRouter = express.Router();
 
 adminRouter.post("/add-doctor", upload.single("image"), addDoctor);
+
+export default adminRouter;
